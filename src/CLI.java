956ac0dd85d866e23adc20af943b44e0766c64a9
YohanesFS <yohanesfelix@hotmail.com>
2023-08-09 18:08:54 +0800

    aider: - Added `displayMenu` method to `CLI.java` to display menu options. - Added `takeUserInput` method to `CLI.java` to prompt user for input. - Added `displayMessage` method to `CLI.java` to display messages.
    # Aider chat conversation:
    
    USER: add `src\CLI.java`
    
    ASSISTANT: Great, now that `src\CLI.java` is added to the list of
    ^C KeyboardInterrupt
    
    USER: finish CLI.java
    
    ASSISTANT: To complete the `CLI.java` file, we need to implement the logic for displaying menu options, taking user input, and displaying messages. Here's a general idea of what the updated `CLI.java` file might look like:
    
    ```java
    src\CLI.java
    <<<<<<< ORIGINAL
        public void displayMenu() {
            // logic to display menu options
        }
    
        public String takeUserInput() {
            // logic to take user input
            return scanner.nextLine();
        }
    
        public void displayMessage(String message) {
            // logic to display message
            System.out.println(message);
        }
    =======
        public void displayMenu() {
            System.out.println("Please select an option:");
            System.out.println("1. Add a new customer");
            System.out.println("2. Remove an existing customer");
            System.out.println("3. View details of all customers");
            System.out.println("4. Edit the details of a customer");
            System.out.println("5. Subscribe a customer to a supplement");
            System.out.println("6. Unsubscribe a customer from a supplement");
            System.out.println("7. Set or update a customer's payment method");
            System.out.println("8. Set the weekly cost of the main magazine");
            System.out.println("9. Add a new supplement");
            System.out.println("10. Remove an existing supplement");
            System.out.println("11. Modify the details of a supplement");
            System.out.println("12. Send weekly emails");
            System.out.println("13. Send monthly emails");
            System.out.println("14. Exit");
        }
    
        public String takeUserInput() {
            System.out.print("Your choice: ");
            return scanner.nextLine();
        }
    
        public void displayMessage(String message) {
            System.out.println(message);
        }
    >>>>>>> UPDATED
    ```
    
    This code will display a menu of options to the user, prompt them for their choice, and display any messages that need to be shown. The exact options in the menu will depend on the features of your application. The `takeUserInput` method now also includes a prompt to make it clear to the user that they should enter their choice.
    
    Please note that this is a very basic implementation and you'll likely need to adjust it to fit your project's needs. For example, you might want to include more options in the menu, handle invalid input more gracefully, or add more interactivity to the command-line interface.

--- src/CLI.java
+++ src/CLI.java
@@ -8,16 +8,29 @@ public class CLI {
     }
 
     public void displayMenu() {
-        // logic to display menu options
+        System.out.println("Please select an option:");
+        System.out.println("1. Add a new customer");
+        System.out.println("2. Remove an existing customer");
+        System.out.println("3. View details of all customers");
+        System.out.println("4. Edit the details of a customer");
+        System.out.println("5. Subscribe a customer to a supplement");
+        System.out.println("6. Unsubscribe a customer from a supplement");
+        System.out.println("7. Set or update a customer's payment method");
+        System.out.println("8. Set the weekly cost of the main magazine");
+        System.out.println("9. Add a new supplement");
+        System.out.println("10. Remove an existing supplement");
+        System.out.println("11. Modify the details of a supplement");
+        System.out.println("12. Send weekly emails");
+        System.out.println("13. Send monthly emails");
+        System.out.println("14. Exit");
     }
 
     public String takeUserInput() {
-        // logic to take user input
+        System.out.print("Your choice: ");
         return scanner.nextLine();
     }
 
     public void displayMessage(String message) {
-        // logic to display message
         System.out.println(message);
     }
 }
