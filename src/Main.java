75a206cfdcd97495c62d99582a8db08ed7b2c519
YohanesFS <yohanesfelix@hotmail.com>
2023-08-09 18:12:02 +0800

    aider: Initialize the `customers` list with 5-6 different customers with built-in details.
    # Aider chat conversation:
    
    USER: proceed
    
    ASSISTANT: Next, we'll implement the `initializeCustomers` method. This method should initialize the `customers` list with 5-6 different customers with built-in details. We can use the `addCustomer` method of the `CustomerManager` class to add customers.
    
    ```java
    src\Main.java
    <<<<<<< ORIGINAL
        public void initializeCustomers() {
            // logic to initialize customers
        }
    =======
        public void initializeCustomers() {
            List<String> interestedSupplements1 = Arrays.asList("Supplement 1", "Supplement 2");
            List<String> interestedSupplements2 = Arrays.asList("Supplement 2", "Supplement 3");
            List<String> interestedSupplements3 = Arrays.asList("Supplement 1", "Supplement 3");
            List<String> interestedSupplements4 = Arrays.asList("Supplement 1");
            List<String> interestedSupplements5 = Arrays.asList("Supplement 2");
            List<String> interestedSupplements6 = Arrays.asList("Supplement 3");
    
            Customer customer1 = new Customer("Customer 1", "customer1@example.com", interestedSupplements1);
            Customer customer2 = new Customer("Customer 2", "customer2@example.com", interestedSupplements2);
            Customer customer3 = new Customer("Customer 3", "customer3@example.com", interestedSupplements3);
            Customer customer4 = new Customer("Customer 4", "customer4@example.com", interestedSupplements4);
            Customer customer5 = new Customer("Customer 5", "customer5@example.com", interestedSupplements5);
            Customer customer6 = new Customer("Customer 6", "customer6@example.com", interestedSupplements6);
    
            this.customers.add(customer1);
            this.customers.add(customer2);
            this.customers.add(customer3);
            this.customers.add(customer4);
            this.customers.add(customer5);
            this.customers.add(customer6);
        }
    >>>>>>> UPDATED
    ```
    
    Please confirm if this is the correct approach for initializing the customers. If it is, we can proceed with the other methods.

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,26 @@ public class Main {
     }
 
     public void initializeCustomers() {
-        // logic to initialize customers
+        List<String> interestedSupplements1 = Arrays.asList("Supplement 1", "Supplement 2");
+        List<String> interestedSupplements2 = Arrays.asList("Supplement 2", "Supplement 3");
+        List<String> interestedSupplements3 = Arrays.asList("Supplement 1", "Supplement 3");
+        List<String> interestedSupplements4 = Arrays.asList("Supplement 1");
+        List<String> interestedSupplements5 = Arrays.asList("Supplement 2");
+        List<String> interestedSupplements6 = Arrays.asList("Supplement 3");
+
+        Customer customer1 = new Customer("Customer 1", "customer1@example.com", interestedSupplements1);
+        Customer customer2 = new Customer("Customer 2", "customer2@example.com", interestedSupplements2);
+        Customer customer3 = new Customer("Customer 3", "customer3@example.com", interestedSupplements3);
+        Customer customer4 = new Customer("Customer 4", "customer4@example.com", interestedSupplements4);
+        Customer customer5 = new Customer("Customer 5", "customer5@example.com", interestedSupplements5);
+        Customer customer6 = new Customer("Customer 6", "customer6@example.com", interestedSupplements6);
+
+        this.customers.add(customer1);
+        this.customers.add(customer2);
+        this.customers.add(customer3);
+        this.customers.add(customer4);
+        this.customers.add(customer5);
+        this.customers.add(customer6);
     }
 
     public void generateEmails() {
